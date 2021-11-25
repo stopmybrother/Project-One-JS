dc20771faf1b877a74b2eef4794112cc3d327c34
Zhenya Aleksandrova <laberfo@gmail.com>
2021-11-25 23:17:16 +0300

    added: change all files

--- js/script1.js
+++ js/script1.js
@@ -22,11 +22,17 @@ clickBtn.addEventListener("click", () => {
 	wrapperItem.innerHTML = "";
 	data.forEach((_item, index, array) => {
 		wrapperItem.innerHTML += `
-    		<p class="wrapper__text">Tittle: ${array[index].tittle}</p>
-    		<p class="wrapper__text">Description: ${array[index].description}</p>
-    		<button class="wrapper__btnEdit">EDIT</button>
-    		<button class="wrapper__btnDelete">DELETE</button>
+			<div class="wrapper__capsule">
+				<div class="wrapper__capsuleText">
+					<p class="wrapper__text">Tittle: ${array[index].tittle}</p>
+					<p class="wrapper__text">Description: ${array[index].description}</p>
+				</div>
+				<div class="wrapper__capsuleBtn">
+					<button class="wrapper__btnEdit">edit</button>
+					<button class="wrapper__btnDelete">delete</button>
+				</div>
+			</div>
 		`;
 	});
+	console.log(data);
 });
-console.log(data);
