1740e8ad3d3cf70452d966e71d629e6bbc76955a
Zhenya Aleksandrova <laberfo@gmail.com>
2021-12-13 22:31:38 +0300

    added: all files

--- js/script1.js
+++ js/script1.js
@@ -1,121 +1,186 @@
-const drawList = (dataType) => {
-	const toDoSection = document.querySelector("#toDo");
+const commonVariables = (eventTarget, data) => {
+	const sectionId = eventTarget.closest(".section").id;
+	const card = eventTarget.closest(".wrapper__card");
+	const cardId = +card.id;
+	const deletedCard = data[sectionId].filter((card) => card.id === cardId)[0];
+	const deletedCardIndex = data[sectionId].findIndex(
+		(card) => card.id === deletedCard.id
+	);
 
-	toDoSection.innerHTML = "";
+	return { sectionId, deletedCard, deletedCardIndex };
+};
 
-	dataType.forEach((item) => {
-		toDoSection.innerHTML += `
-			<div class="wrapper__toDoItem">
-				<div class="wrapper__toDoItemFull">
+const drawList = (data, sectionType) => {
+	const section = document.querySelector(`#${sectionType}`);
+
+	section.innerHTML = "";
+
+	data[sectionType].forEach((item) => {
+		section.innerHTML += `
+			<div class="wrapper__card" id=${item.id}>
+				<div class="wrapper__toDoListItemFull">
 					<p class="wrapper__tittleName">Tittle:</p>
 					<p class="wrapper__tittle">${item.tittle}</p>
 					<p class="wrapper__descriptionName">Decscription:</p>
 					<p class="wrapper__description">${item.description}</p>
 				</div>
-				<div class="wrapper__toDoBtn">
-					<button class="wrapper__btnEdit">edit</button>
-					<button class="wrapper__btnDelete">delete</button>
-				</div>
-				<button class="wrapper__BtnBegin">begin</button>
+				${
+					sectionType !== "deleted"
+						? `
+						<div class="wrapper__toDoListBtn">
+							<div class="wrapper__editAndDeleteBtns">
+								<button class="wrapper__btnEdit">edit</button>
+								<button class="wrapper__btnDelete">delete</button>
+							</div>
+							<button class="wrapper__btnNext">next</button>
+						</div>
+					`
+						: `
+						<button class="wrapper__btnRestore">restore</button>
+					`
+				}
 			</div>
 		`;
 	});
 };
 
-const deleteCard = (dataType, tittle, description) => {
-	dataType.forEach((item, index) => {
-		if (item.tittle === tittle && item.description === description) {
-			dataType.splice(index, 1);
-		}
+const createCard = (data) => {
+	const form = document.querySelector("#form");
+	const inputTittle = document.querySelector("#inputTittle");
+	const inputDescription = document.querySelector("#inputDescription");
+
+	data.toDo.push({
+		tittle: inputTittle.value,
+		description: inputDescription.value,
+		id: Date.now(),
 	});
-	drawList(dataType);
+
+	form.reset();
+
+	drawList(data, "toDo");
 };
 
-const editCard = (dataType, tittle, description) => {
+const deleteCard = (eventTarget, data) => {
+	const { sectionId, deletedCard, deletedCardIndex } = commonVariables(
+		eventTarget,
+		data
+	);
+
+	data[sectionId].splice(deletedCardIndex, 1);
+	data.deleted.push(deletedCard);
+
+	drawList(data, sectionId);
+	drawList(data, "deleted");
+};
+
+const editCard = (eventTarget, data) => {
+	const { sectionId, deletedCard, deletedCardIndex } = commonVariables(
+		eventTarget,
+		data
+	);
+
 	const modalWrapper = document.querySelector(".wrapper__modalGeneralWrapper");
-	modalWrapper.style.display = "block";
-
-	const closeButton = document.querySelector("#closeButton");
-	closeButton.addEventListener("click", () => {
-		modalWrapper.style.display = "none";
-	});
-
 	const tittleInModal = document.querySelector(".wrapper__modalInputTittle");
 	const descriptionInModal = document.querySelector(
 		".wrapper__modalInputDescription"
 	);
 
-	tittleInModal.value = tittle;
-	descriptionInModal.value = description;
-
+	const closeButton = document.querySelector(".wrapper__roundButton");
 	const submitButton = document.querySelector(".wrapper__submitButton");
+
+	modalWrapper.style.display = "block";
+
+	tittleInModal.value = deletedCard.tittle;
+	descriptionInModal.value = deletedCard.description;
 
 	submitButton.addEventListener("click", (event) => {
 		event.preventDefault();
 
-		dataType.forEach((item, index) => {
-			if (item.tittle === tittle && item.description === description) {
-				dataType.splice(index, 1, {
-					tittle: tittleInModal.value,
-					description: descriptionInModal.value,
-				});
-			}
+		data[sectionId].splice(deletedCardIndex, 1, {
+			tittle: tittleInModal.value,
+			description: descriptionInModal.value,
+			id: deletedCard.id,
 		});
-
 		modalWrapper.style.display = "none";
-		drawList(dataType);
+
+		drawList(data, sectionId);
+	});
+
+	closeButton.addEventListener("click", (event) => {
+		event.preventDefault();
+		modalWrapper.style.display = "none";
 	});
 };
 
+const transferCardToAnotherSection = (eventTarget, data) => {
+	const { sectionId, deletedCard, deletedCardIndex } = commonVariables(
+		eventTarget,
+		data
+	);
+	const sectionsId = [...document.querySelectorAll(".section")].map(
+		(section) => section.id
+	);
+	const nextSectionIndex = sectionsId.findIndex((id) => id === sectionId) + 1;
+
+	data[sectionId].splice(deletedCardIndex, 1);
+	data[sectionsId[nextSectionIndex]].push(deletedCard);
+
+	drawList(data, sectionId);
+	drawList(data, sectionsId[nextSectionIndex]);
+};
+
+const restoreCard = (eventTarget, data) => {
+	const { sectionId, deletedCard, deletedCardIndex } = commonVariables(
+		eventTarget,
+		data
+	);
+
+	data[sectionId].splice(deletedCardIndex, 1);
+	data.toDo.push(deletedCard);
+
+	drawList(data, sectionId);
+	drawList(data, "toDo");
+};
+
+const emptyTrash = (data) => {
+	data.deleted = [];
+	drawList(data, "deleted");
+};
+
 const init = () => {
-	const toDoList = document.querySelector(".wrapper__toDoList");
-	const form = document.querySelector("#form");
-	const inputTittle = document.querySelector("#inputTittle");
-	const inputDescription = document.querySelector("#inputDescription");
+	const toDoList = document.querySelector("#toDoList");
 	const clickBtn = document.querySelector("#clickBtn");
+	const emptyTrashBtn = document.querySelector(".header__emptyTrashBtn");
 
 	const data = {
 		toDo: [],
 		inProgress: [],
 		done: [],
+		deleted: [],
 	};
 
 	clickBtn.addEventListener("click", (event) => {
 		event.preventDefault();
 
-		data.toDo.push({
-			tittle: inputTittle.value,
-			description: inputDescription.value,
-		});
-
-		form.reset();
-
-		drawList(data.toDo);
+		createCard(data);
 	});
 
 	toDoList.addEventListener("click", (event) => {
-		switch (event.target.classList.value) {
-			case "wrapper__btnDelete":
-				const card = event.target.closest(".wrapper__toDoItem");
-				const tittle = card.querySelector(".wrapper__tittle").textContent;
-				const description = card.querySelector(".wrapper__description").textContent;
-
-				deleteCard(data.toDo, tittle, description);
+		switch (true) {
+			case [...event.target.classList].includes("wrapper__btnDelete"):
+				deleteCard(event.target, data);
 				break;
-
-			case "wrapper__btnEdit":
-				const cardForModal = event.target.closest(".wrapper__toDoItem");
-				const tittleForModal =
-					cardForModal.querySelector(".wrapper__tittle").textContent;
-				const descriptionForModal = cardForModal.querySelector(
-					".wrapper__description"
-				).textContent;
-				editCard(data.toDo, tittleForModal, descriptionForModal);
+			case [...event.target.classList].includes("wrapper__btnEdit"):
+				editCard(event.target, data);
 				break;
-			case "wrapper__BtnBegin":
-				
+			case [...event.target.classList].includes("wrapper__btnNext"):
+				transferCardToAnotherSection(event.target, data);
 				break;
-			default:
+			case [...event.target.classList].includes("wrapper__btnRestore"):
+				restoreCard(event.target, data);
+				break;
+			case [...event.target.classList].includes("wrapper__emptyTrashBtn"):
+				emptyTrash(data);
 				break;
 		}
 	});
